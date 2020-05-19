9a7125408b6e1ef8bfbf7204b814f57ba2711e28
NicoFrsh <nfrisch@unal.edu.co>
2020-05-19 17:31:13 -0500

    Comments

--- run_analysis.R
+++ run_analysis.R
@@ -20,18 +20,6 @@ rm(names)
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
 
-# what exactly are the subject and y tables?
-unique(subject_test$V1)
-length(unique(subject_test$V1))
-
-unique(subject_train$V1)
-length(unique(subject_train$V1))
-# as there are 9 different subject_test object it seems those are the IDs 
-# of the 9 (30% of 30) test persons and 21 train persons
-unique(y_test$V1)
-unique(y_train$V1)
-# the numbers represent the 6 different activities: WALKING, etc
-
 # rename subject and y table columns
 names(subject_train) <- "subject"; names(subject_test) <- "subject"
 names(y_test) <- "activity"; names(y_train) <- "activity"
